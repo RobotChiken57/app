5a294eebd4eba63c4f01fe88ca2e52fd2a622057
RobotChiken57 <99732056+RobotChiken57@users.noreply.github.com>
2023-04-19 08:16:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function generateFibonacciSequence(n) {
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+      sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence;
+  }
+  
+  function generateSequence() {
+    let num = document.getElementById("num").value;
+    let sequence = generateFibonacciSequence(num);
+    document.getElementById("sequence").innerHTML = sequence.join(", ");
+}
