a503eca3c35c5c719ed362d5b621eb8c14ad2ec9
RobotChiken57 <99732056+RobotChiken57@users.noreply.github.com>
2023-04-26 07:53:40 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function generateFibonacciSequence(n) {
-    let sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-      sequence[i] = sequence[i - 1] + sequence[i - 2];
-    }
-    return sequence;
+  if (n < 0) {
+    throw new Error("Число должно быть положительным!");
   }
-  
-  function generateSequence() {
-    let num = document.getElementById("num").value;
-    let sequence = generateFibonacciSequence(num);
-    document.getElementById("sequence").innerHTML = sequence.join(", ");
+  let sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  return sequence;
 }
+
+function generateSequence() {
+  let num = document.getElementById("num").value;
+  if (num < 0) {
+    document.getElementById("sequence").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
+    alert("Число должно быть положительным!");
+    return;
+  }
+  let sequence = generateFibonacciSequence(parseInt(num));
+  document.getElementById("sequence").innerHTML = sequence.join(", ");
+  
+  let index = document.getElementById("index").value;
+  if (index !== "") {
+      let result = sequence[parseInt(index)];
+      document.getElementById("result").innerHTML = result;
+  }
+}
+
+
